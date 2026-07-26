28ec116eca39e4c9829660552ab363bca515acbf
bis4mark <60272051+bis4mark@users.noreply.github.com>
2026-07-26 16:11:17 +0000

    Add src/App.js component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>AltOps Workshop</h1>
+        <p>Welcome to the React Application</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
